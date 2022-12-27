e438790e21e16bda7fb676a390740b4d5ab3f29c
German Loredo Becerra <german@vauxoo.com>
2022-12-27 17:08:34 -0600

    Chapter 3

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "XM Module",
+    'version': '1.0',
+    'depends': [
+        'base',
+    ],
+    'author': "German",
+    'category': 'Customs',
+    'description': """
+    This is the first module
+    """,
+    # data files always loaded at installation
+    'data': [],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+    'application' : True,
+}
