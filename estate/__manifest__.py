2651f6d5a0eb1415a1b77ec0447b3f50af2caf4b
German Loredo Becerra <german@vauxoo.com>
2022-12-27 21:56:13 -0600

    Capitulo 6 actions

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -13,6 +13,7 @@
     # data files always loaded at installation
     'data': [
         "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
     ],
     # data files containing optionally loaded demonstration data
     'demo': [],
