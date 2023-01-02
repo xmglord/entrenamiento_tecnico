316f3399e723fe9511bac4a849c421348b420959
German Loredo Becerra <german@vauxoo.com>
2023-01-02 16:00:30 -0600

    Capitulo 12 stat buttons

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -23,6 +23,8 @@ class EstatePropertyOffer(models.Model):
     property_id = fields.Many2one("estate.property", string="Property", required=True)
     validity = fields.Integer(string="Validity (days)", default=7)
     date_deadline = fields.Date(string="Deadline", compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+    property_type_id = fields.Many2one("estate.property.type", related="property_id.property_type_id",\
+            string="Property Type", store=True)
 
     @api.depends("create_date", "validity")
     def _compute_date_deadline(self):
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -11,3 +11,17 @@ class EstatePropertyType(models.Model):
     name = fields.Char("Name", required=True)
     property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
     sequence = fields.Integer("Sequence", default = 10)
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id", string="Offers")
+    offer_count = fields.Integer(string="Offers Count", compute="_compute_offer")
+
+    def _compute_offer(self):
+        data = self.env["estate.property.offer"].read_group(
+                [("property_id.state", "!=", "canceled"), ("property_type_id", "!=", False)],
+                ["ids:array_agg(id)", "property_type_id"],
+            ["property_type_id"],
+        )
+        mapped_count = {d["property_type_id"][0]: d["property_type_id_count"] for d in data}
+        mapped_ids = {d["property_type_id"][0]: d["ids"] for d in data}
+        for prop_type in self:
+            prop_type.offer_count = mapped_count.get(prop_type.id, 0)
+            prop_type.offer_ids = mapped_ids.get(prop_type.id, [])
