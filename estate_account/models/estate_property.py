f1df4558ccaff315ca7fddccd28f7b604569bff8
German Loredo Becerra <german@vauxoo.com>
2023-01-03 19:15:23 -0600

    Capitulo 14 Invoice Creation Adicionalmente se arreglo un bug que existia en la visualizacion de estate.property, el boton "Cancel" desaparecia al recibir una oferta.

---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,28 @@
+from odoo import models, Command
+from odoo.exceptions import UserError
+
+class EstateProperty(models.Model):
+    _inherit = "estate.property"
+
+    def action_sold(self):
+        res = super().action_sold()
+        for prop in self:
+            self.env["account.move"].create(
+                {
+                    "partner_id": prop.buyer_id.id,
+                    "move_type": "out_invoice",
+                    "invoice_line_ids": [
+                        Command.create({
+                            "name": prop.name,
+                            "quantity": 1.0,
+                            "price_unit": prop.selling_price * 6.0 / 100.0,
+                        }),
+                        Command.create({
+                            "name": "Administrative fees",
+                            "quantity": 1.0,
+                            "price_unit": 100.0,
+                        }),
+                    ],
+                }
+            )
+        return res
