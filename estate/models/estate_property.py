b09f45806baaa5dd6258c81caa7cf730c0a4df5d
German Loredo Becerra <german@vauxoo.com>
2023-01-08 15:03:31 -0600

    Advanced B multi-copany security Se hicieron los ejercios, sin emabrgo, en el codigo se comenta que se tuvo que implementar de forma distinta dado que las reglas de interponian y aun despues de buscar de varias formas se seguian aplicando las reglas con un OR entre ellas cuando se necesitaba que se aplicara un AND

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -44,6 +44,7 @@ class EstateProperty(models.Model):
     )
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
     user_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
+    company_id = fields.Many2one("res.company", string="Company", required=True, default=lambda self: self.env.company)
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
